3cb37ee486ae2596ce3993fe0134577021b0e23d
bassem <balamddine@gmail.com>
2015-10-21 13:56:26 +0300

    First commit

---
+++ Scripts/KeyStatus.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    window.keydown = {};
+    window.keyup = {};
+    function keyName(event) {
+        return jQuery.hotkeys.specialKeys[event.which] ||
+String.fromCharCode(event.which).toLowerCase();
+    }
+    $(document).bind("keydown", function (event) {
+        keydown[keyName(event)] = true;
+    });
+    $(document).bind("keyup", function (event) {
+        keydown[keyName(event)] = false;
+        keyup[keyName(event)] = true;
+    });
+}); 
